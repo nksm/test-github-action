b9d33f592a150d3b9c290e2627a8f18b293e6a49
nksm <nksm@ness.gr.jp>
2021-01-14 14:59:23 +0900

    fix lint

--- test/src/App.vue
+++ test/src/App.vue
@@ -26,11 +26,11 @@ export default {
 
 <style lang="scss" scoped>
 #app {
+  display: block;
   padding: 10px;
   margin-top: 60px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   color: #2cc;
-  display: block;
   text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
