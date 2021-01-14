dc0b5cc7f054de68b9869fece5348ae9d9f9de0d
nksm <nksm@ness.gr.jp>
2021-01-14 14:51:18 +0900

    fix lint

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,37 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./image/assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./image/assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
   },
   created() {
-    console.log('test');
-
+    console.log("test");
   },
   methods: {
-    test(){
-      console.log('test')
-    }
+    test() {
+      console.log("test");
+    },
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
 #app {
-  display: block;
   padding: 10px;
   margin-top: 60px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   color: #2cc;
+  display: block;
   text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
