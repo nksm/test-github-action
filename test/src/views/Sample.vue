09fc897de874b7190558f55cdb895f9fde7b8a51
nksm <nksm@ness.gr.jp>
2021-01-18 09:49:14 +0900

    add sample view

---
+++ test/src/views/Sample.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/image/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  created() {
+    console.log("test");
+  },
+  methods: {
+    test() {
+      console.log("test")
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#app {
+  padding: 10px;
+  margin-top: 60px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  display: block;
+  color: #2cc;
+  text-align: center;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
